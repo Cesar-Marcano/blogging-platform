e9873a202dddaee1347306831878e80a9b94757d
Cesar-Marcano <marcanocesar86@gmail.com>
2025-03-07 20:43:50 -0400

    feat(auth): implement Kafka microservice transport for authentication

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092'],
+        },
+      },
+    },
+  );
+  await app.listen();
 }
-bootstrap();
+
+void bootstrap();
