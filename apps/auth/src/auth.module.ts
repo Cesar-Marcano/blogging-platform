d19d3524db2d42e61675c4c98c0b38882f7ee75a
Cesar-Marcano <marcanocesar86@gmail.com>
2025-03-07 22:40:48 -0400

    chore(auth): move session logic into auth microservice and remove unnecessary session microservice

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
+import { SessionModule } from './session/session.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), SessionModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
